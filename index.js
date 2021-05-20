112186176e43758b961d9a43d33a44623d5c286e
Oscar Shue <oscarshue@hotmail.co.uk>
2021-05-20 13:04:02 +0100

    Added .env file, sorted navigation

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+
 app.set("view engine", "ejs");
 
 // To access photos from the 'public' folder uncomment below line
 //app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+const { WEB_PORT } = process.env;
+
+app.get("/home", (req, res) => {
   res.render("index");
+});
+
+app.get("/drivers", (req, res) => {
+  res.render("drivers");
 });
 
 app.listen(port, () => {
